5972308d0fc785e08afb9db6320f20d0b4d4197c
paulhunter <p.hunter.eng@gmail.com>
2013-02-10 00:08:45 -0800

    Problem 36 Solution, ~4.5 second Execution Time

--- p36.py
+++ p36.py
@@ -1,11 +1,18 @@
+'''
+The decimal number, 585 = 1001001001 (binary), is palindromic in both bases.
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+(Please note that the palindromic number, in either base, may not include leading zeros.)
 
+Note:
+Should be relatively straight forward
+'''
 
 import time
 
 def main():
 	t = time.clock()
 	sum = 0
-	for i in range(2,1000000):
+	for i in range(1000000):
 		if (isPalidrome(i) and isPalidrome(toBinary(i))):
 			sum += i
 	print "Answer: %d" % sum
@@ -16,7 +23,7 @@ def isPalidrome(x):
 	if len(array) == 1:
 		return True
 	firstHalf = array[:len(array)/2]
-	secondHalf = (array[len(array)/2:])
+	secondHalf = (array[len(array)/2+(1 if len(array)%2==1 else 0):])
 	secondHalf.reverse()
 	return firstHalf == secondHalf
 
