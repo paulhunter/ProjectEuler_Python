cc13dd9a3bc2df797df42ea72516fbcdc1ece956
paulhunter <p.hunter.eng@gmail.com>
2013-02-13 18:50:32 -0800

    Problem 49 Solution, ~0.050 second Execution Time

--- p49.py
+++ p49.py
@@ -6,29 +6,41 @@ There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, ex
 What 12-digit number do you form by concatenating the three terms in this sequence?
 
 Notes:
-We can save time by only checking the differences of primes after generating primes up to 9999
-which is the largest 4 digit number. From there we can then check each 4 digit prime with 
-the 4 digit primes that are less than themselves to find if their next term would be prime. 
+With each prime, find all of the permuations that are prime, then find if there are three
+that constitute a sequence and that is not (1487,4817,8147)
 '''
 import p27
+import p32
+import itertools
 import time
 
 def main():
 	t = time.clock()
-	print "Answer: %d" % findSeq()[0]
+	print "Answer: %s" % findSeq()
 	print "Time Taken: %0.3f seconds" % (time.clock()-t)
 
 def findSeq():
 	primeCat = p27.findPrimes(9999)
 	primes = [x for x in primeCat.keys() if primeCat[x] == True and x>999 and x<10000]
+	res = []
 	for p in primes:
-		for q in primes:
-			if q>=p:
-				break
-			elif 2*p-q>9999:
+		res = []
+		for t in [x for x in itertools.permutations(p.__str__(),4)]:
+			if primeCat[p32.convertToInt(t)] == True and p32.convertToInt(t) != p and len(p32.convertToInt(t).__str__())==4:
+				res.append(p32.convertToInt(t))
+		for r in res:
+			if r in (1487,4817,8147):
 				continue
-			elif primeCat[2*p-q] == True:
-				return (q,p,2*p-q)
+			for s in res:
+				if s >= r:
+					break
+				if (2*r-s) in res:
+					st = ''
+					for s in (s,r,2*r-s): st+=str(s)
+					return st
+
+
+
 
 if __name__ == '__main__':
 	main()
